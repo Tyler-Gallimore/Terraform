88327f621a538eb20cb214a7c4aa3c17e84e1a65
Tyler-Gallimore <lifforn@mailfence.com>
2023-07-25 00:03:51 +0000

    Terraform EC2 instance creation

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "InstanceName" {
+    ami = "ami-05548f9cecf47b442"
+    instance_type = "t2.micro"
+    
+    tags = {
+        Name = "HelloWorld"
+    }
+}
+
+#Notes:
+#Use terraform validate to check code before deployment
+#terraform write is creating the code and pushing to Github
+#terraform init once configeration file is created
+#terraform plan shows what will be performed
+#terraform apply to execute the code
