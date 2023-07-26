c11a135cbae28eb9081a4be98c5f0a1fa8bef4cf
Tyler-Gallimore <129459190+Tyler-Gallimore@users.noreply.github.com>
2023-07-25 20:37:11 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,8 @@ provider "aws" {
 resource "aws_instance" "InstanceName" {
     ami = "ami-05548f9cecf47b442"
     instance_type = "t2.micro"
-    
+    #subnet_id = "public subnet"
+    #vpc_security_group_ids = "SG id"
     tags = {
         Name = "HelloWorld"
     }
