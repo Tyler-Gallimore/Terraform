b50b62ead2a116875751b6d5107daac909b1cfa9
Tyler-Gallimore <129459190+Tyler-Gallimore@users.noreply.github.com>
2023-07-27 21:48:13 -0400

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,15 @@
 terraform {
-  required_version = "= 1.0.0"
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    http = {
+      source = "hashicorp/http"
+      version = "2.1.0"
+    }
+  }
 }
 
 #Used to pin a certain version of terraform
