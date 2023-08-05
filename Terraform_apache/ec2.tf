3d3572e38387a1b1bc96b638a2862effb19c0fdf
Tyler-Gallimore <129459190+Tyler-Gallimore@users.noreply.github.com>
2023-08-05 19:03:12 -0400

    Update ec2.tf

--- Terraform_apache/ec2.tf
+++ Terraform_apache/ec2.tf
@@ -20,6 +20,6 @@ resource "aws_launch_template" "apache_web" {
   image_id               = var.ami
   instance_type          = "t2.micro"
   key_name               = data.aws_key_pair.existing_key_pair.key_name
-  user_data              = var.user_data
+  user_data              = var.user_data #Requires base64 code
   vpc_security_group_ids = [aws_security_group.apache.id]
 }
