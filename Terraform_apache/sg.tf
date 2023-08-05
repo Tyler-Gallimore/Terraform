b21e0e386977796bb800d08c1986a3b88f1381e6
Tyler-Gallimore <129459190+Tyler-Gallimore@users.noreply.github.com>
2023-08-05 18:36:09 -0400

    Update sg.tf

--- Terraform_apache/sg.tf
+++ Terraform_apache/sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "apache" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["35.145.180.2/32"]
+    cidr_blocks = ["IP Address"]
   }
 
   ingress {
